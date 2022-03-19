6c042717d4bb24f5ce6e28985bd92397286151fd
xuboHaven <xubo_github123>
2022-03-19 17:20:41 +0800

    20220316 1437

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(DemoApplication.class, args);
     }
 
